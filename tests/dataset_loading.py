28bae4ea5a0dd3b0257144d31767cc4f702e195b
Filippo Ziche <filippo.ziche@gmail.com>
2024-01-15 23:47:22 +0100

    adds test for dataframe to tensor samples conversion

--- tests/dataset_loading.py
+++ tests/dataset_loading.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+import torch
 
 EXERCISE = 1
 FEATURES = ['pos_x', 'pos_y', 'pos_z']
+
+samples = []
 
 # Load samples from processed dataset
 samples_path = 'data/processed/kimore_samples.parquet.gzip'
@@ -10,13 +13,27 @@ df = pd.read_parquet(samples_path)
 
 # Keeps only data of exercise 1
 samples_df = df[df['exercise'] == EXERCISE]
-samples_df = samples_df.drop(columns=['exercise'])
 
 # Create samples for training
 grouped = samples_df.groupby(['type', 'subject'])
 for _, group in grouped:
-    sample = group.drop(columns=['type', 'subject'])
-    sample['features'] = sample[FEATURES].values.tolist()
-    sample = sample.drop(columns=FEATURES).reset_index(drop=True)
-    print(sample)
 
+    # Keeps only features and index by frame and joint id
+    group.set_index(['frame', 'joint'], inplace=True)
+    group = group[FEATURES]
+
+    # Get dimensionality
+    frames_count = len(group.index.get_level_values(0).unique())
+    joints_count = len(group.index.get_level_values(1).unique())
+
+    try:
+        result = np.reshape(group, (frames_count, joints_count, 3))
+        samples.append(result)
+    except:
+        print("ERROR CONVERTING!")
+        print(group)
+        input()
+
+print(f'samples: {len(samples)}')
+for sample in samples:
+    print(f'\tsize: {sample.shape}')
